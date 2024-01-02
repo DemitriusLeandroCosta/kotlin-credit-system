080ec2692676f5f558bf651ccfb499bcf3822011
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-02 17:14:18 -0300

    criação das classes de dominio

---
+++ src/main/java/com/aideia/creditapplicationsystem/entity/Customer.kt
@@ -0,0 +1,3 @@
+package com.aideia.creditapplicationsystem.entity
+
+data class Customer()
---
+++ src/main/java/com/aideia/creditapplicationsystem/enumeration/Status.kt
@@ -0,0 +1,5 @@
+package com.aideia.creditapplicationsystem.entity
+
+enum class Status {
+    IN_PROGRESS,APPROVED, REJECT
+}
