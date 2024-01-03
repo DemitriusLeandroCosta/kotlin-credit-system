993e0c15cc74bbb1439ca9abcf4f0f210017fccd
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-03 09:53:25 -0300

    Adição de comentário

--- src/main/java/com/aideia/creditapplicationsystem/repository/CustomerRepository.kt
+++ src/main/java/com/aideia/creditapplicationsystem/repository/CustomerRepository.kt
@@ -4,6 +4,7 @@ import com.aideia.creditapplicationsystem.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+//Criação da interface
 @Repository
 interface CustomerRepository: JpaRepository<Customer, Long> {
 }
