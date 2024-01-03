eb4cc9eac73eacc2c4cc8eae4f878dbedb8c78ff
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-03 09:49:14 -0300

    criação das Interfaces Repository - Credit e Customer

---
+++ src/main/java/com/aideia/creditapplicationsystem/repository/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package com.aideia.creditapplicationsystem.repository
+
+import com.aideia.creditapplicationsystem.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository: JpaRepository<Customer, Long> {
+}
