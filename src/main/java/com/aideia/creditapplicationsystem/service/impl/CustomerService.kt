b553e6844672df2125c66c57ed5ad40c3cb12c22
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-03 13:55:11 -0300

    Criação da Classe CustomerService, com a interface ICustomerService

---
+++ src/main/java/com/aideia/creditapplicationsystem/service/impl/CustomerService.kt
@@ -0,0 +1,18 @@
+package com.aideia.creditapplicationsystem.service.impl
+
+import com.aideia.creditapplicationsystem.entity.Customer
+import com.aideia.creditapplicationsystem.service.ICustomerService
+
+class CustomerService: ICustomerService {
+    override fun save(customer: Customer): Customer {
+        TODO("Not yet implemented")
+    }
+
+    override fun finById(id: Long): Customer {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(id: Long): Customer {
+        TODO("Not yet implemented")
+    }
+}
