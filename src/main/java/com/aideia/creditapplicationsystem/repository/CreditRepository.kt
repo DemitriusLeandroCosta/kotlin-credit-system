5a6056bf53c473d88a67aa59339d171021b74959
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-04 11:17:41 -0300

    Implementação da função: findAllByCustomerId

--- src/main/java/com/aideia/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/java/com/aideia/creditapplicationsystem/repository/CreditRepository.kt
@@ -1,11 +1,16 @@
 package com.aideia.creditapplicationsystem.repository
 
 import com.aideia.creditapplicationsystem.entity.Credit
+import jakarta.persistence.Id
 import org.aspectj.apache.bcel.classfile.Code
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.util.UUID
 
 
 interface CreditRepository : CrudRepository<Credit,Long>{
     fun findByCreditCode(creditCode: UUID): Credit?
+
+    @Query(value = "SELECT * from CREDIT where CREDIT_ID = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
 }
