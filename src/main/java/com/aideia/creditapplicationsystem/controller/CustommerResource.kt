003a37f45354d3b63b3b2f89e0f9357aa657ded0
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-04 14:52:32 -0300

    Implementação da função findById para retornar os dados via metodo Get

--- src/main/java/com/aideia/creditapplicationsystem/controller/CustommerResource.kt
+++ src/main/java/com/aideia/creditapplicationsystem/controller/CustommerResource.kt
@@ -1,7 +1,11 @@
 package com.aideia.creditapplicationsystem.controller
 
 import com.aideia.creditapplicationsystem.dto.CustomerDto
+import com.aideia.creditapplicationsystem.dto.CustomerView
+import com.aideia.creditapplicationsystem.entity.Customer
 import com.aideia.creditapplicationsystem.service.impl.CustomerService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,5 +20,11 @@ class CustommerResource(
     fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
        val saveCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${saveCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id:Long): CustomerView {
+      var customer:Customer = this.customerService.finById(id)
+        return CustomerView(customer)
     }
 }
---
+++ src/main/java/com/aideia/creditapplicationsystem/dto/CustomerView.kt
@@ -0,0 +1,25 @@
+package com.aideia.creditapplicationsystem.dto
+
+import com.aideia.creditapplicationsystem.entity.Customer
+import java.math.BigDecimal
+
+data class CustomerView(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val zipCode: String,
+    val street: String
+) {
+    constructor(customer: Customer) : this (
+        firstName = customer.firstName,
+        lastName = customer.lastName,
+        cpf = customer.cpf,
+        income = customer.income,
+        email = customer.email,
+        zipCode = customer.address.zipeCode,
+        street = customer.address.street
+    )
+
+}
