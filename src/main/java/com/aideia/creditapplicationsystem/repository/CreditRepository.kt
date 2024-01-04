76171a642c31ba630ba5490336003e984363cc79
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-04 10:18:26 -0300

    Criação da função findByCreditCode na classe CreditRepository

--- src/main/java/com/aideia/creditapplicationsystem/repository/CreditRepository.kt
+++ src/main/java/com/aideia/creditapplicationsystem/repository/CreditRepository.kt
@@ -1,8 +1,11 @@
 package com.aideia.creditapplicationsystem.repository
 
 import com.aideia.creditapplicationsystem.entity.Credit
+import org.aspectj.apache.bcel.classfile.Code
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
 
 interface CreditRepository : CrudRepository<Credit,Long>{
+    fun findByCreditCode(creditCode: UUID): Credit
 }
