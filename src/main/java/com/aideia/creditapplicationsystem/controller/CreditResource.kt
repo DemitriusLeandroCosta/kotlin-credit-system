c977e264793d4ffafe32da510b5e02902e1832ae
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-05 15:19:25 -0300

    Implementação da função: findByCreditCode

--- src/main/java/com/aideia/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/java/com/aideia/creditapplicationsystem/controller/CreditResource.kt
@@ -1,17 +1,21 @@
 package com.aideia.creditapplicationsystem.controller
 
 import com.aideia.creditapplicationsystem.dto.CreditDto
+import com.aideia.creditapplicationsystem.dto.CreditView
 import com.aideia.creditapplicationsystem.dto.CreditViewList
 import com.aideia.creditapplicationsystem.dto.CustomerDto
 import com.aideia.creditapplicationsystem.entity.Credit
 import com.aideia.creditapplicationsystem.service.impl.CreditService
 import jakarta.persistence.Id
+import org.aspectj.apache.bcel.classfile.Code
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import java.util.stream.Collectors
 
 @RestController
@@ -29,4 +33,12 @@ class CreditResource(
         return this.creditService.findAllByCustomer(customerId).stream()
             .map {credit:Credit -> CreditViewList(credit)}.collect(Collectors.toList())
     }
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value= "customerId") customerId: Long,
+                         @PathVariable creditCode: UUID): CreditView {
+      val credit: Credit =  this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
+    }
+
 }
