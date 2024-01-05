948658ac8cc077e74edf8ca65aa7191a5a7b6a1a
DemitriusLeandroCosta <demitriuscostagithub@gmail.com>
2024-01-05 16:42:09 -0300

    Refactory - Implementação do ResponseEntity - CustomerResouce

--- src/main/java/com/aideia/creditapplicationsystem/controller/CustommerResource.kt
+++ src/main/java/com/aideia/creditapplicationsystem/controller/CustommerResource.kt
@@ -5,6 +5,8 @@ import com.aideia.creditapplicationsystem.dto.CustomerUpdateDto
 import com.aideia.creditapplicationsystem.dto.CustomerView
 import com.aideia.creditapplicationsystem.entity.Customer
 import com.aideia.creditapplicationsystem.service.impl.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -21,26 +23,28 @@ class CustommerResource(
     private val customerService: CustomerService
 ) {
     @PostMapping
-    fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
-       val saveCustomer = this.customerService.save(customerDto.toEntity())
-        return "Customer ${saveCustomer.email} saved!"
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): ResponseEntity<String> {
+        val saveCustomer = this.customerService.save(customerDto.toEntity())
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer ${saveCustomer.email} saved!")
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id:Long): CustomerView {
-      var customer:Customer = this.customerService.finById(id)
-        return CustomerView(customer)
+    fun findById(@PathVariable id: Long): ResponseEntity<CustomerView> {
+        var customer: Customer = this.customerService.finById(id)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customer))
     }
 
     @DeleteMapping("/{id}")
-    fun deleteCustomer(@PathVariable id:Long) = this.customerService.delete(id)
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
 
     @PatchMapping
-    fun updateCustomer(@RequestParam(value = "customerId") id: Long,
-                       @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView{
-       val customer: Customer =  this.customerService.finById(id)
-       val customerToUpdate: Customer =  customerUpdateDto.toEntity(customer)
-       val customerUptaded : Customer =  this.customerService.save(customerToUpdate)
-        return CustomerView(customerUptaded)
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): ResponseEntity<CustomerView> {
+        val customer: Customer = this.customerService.finById(id)
+        val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+        val customerUptaded: Customer = this.customerService.save(customerToUpdate)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customerUptaded))
     }
 }
